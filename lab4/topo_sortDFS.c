c4993aa9e5e412c1f6d80d5850a124f3fee8632a
RaghavendraN-cs213 <153651594+RaghavendraN-cs213@users.noreply.github.com>
2024-06-23 11:16:40 +0530

    Update topo_sortDFS.c

--- lab4/topo_sortDFS.c
+++ lab4/topo_sortDFS.c
@@ -1,62 +1,45 @@
 #include <stdio.h>
-#define v 100
+#include <stdlib.h>
 
-int j=0;
-void dfs(int a_matrix[v][v],int n,int visited[],int start,int res[])
-{
-
-
-        visited[start]=1;
-        for(int i=0;i<n;i++)
-        {
-            if(a_matrix[start][i]==1&& visited[i]==0 )
-            {
-                dfs(a_matrix,n,visited,i,res);
-            }
-        }
-
-    res[j++]=start;
-
-
-}
-void toposort(int a_matrix,int n)
-{
-    int visited[v]={0};
-    int res[v];
-    j=0;
-    for(int i=0;i<n;i++)
-    {
-        if(visited[i]==0)
-        {
-            dfs(a_matrix,n,visited,i,res);
+void DFS(int u, int n, int a[n][n], int s[], int *j, int res[]) {
+    s[u] = 1;
+    for (int v = 0; v < n; v++) {
+        if (a[u][v] == 1 && s[v] == 0) {
+            DFS(v, n, a, s, j, res);
         }
     }
-    printf("the topological sort:");
-    for(int i=n-1;i>=0;i--)
-    {
-        printf("%d",res[i]);
-    }
-
-
+    (*j)++;
+    res[*j] = u;
 }
 
-int main()
-{
-    int a_matrix[v][v];
-    int n;
+void topological_order(int n, int a[n][n]) {
+    int s[n];
+    for (int i = 0; i < n; i++) {
+        s[i] = 0;
+    }
+    int j = -1;
+    int res[n];
 
-    printf("enter the no of vertices:");
-    scanf("%d",&n);
-    printf("enter the adjaceny matrix:\n");
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            scanf("%d",&a_matrix[i][j]);
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 0) {
+            DFS(i, n, a, s, &j, res);
         }
     }
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%d ", res[i]);
+    }
+    printf("\n");
+}
 
-
-    toposort(a_matrix,n);
+int main() {
+    int adjacency_matrix[5][5] = {
+        {0, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 1},
+        {0, 0, 1, 0, 1},
+        {0, 1, 1, 0, 0}
+    };
+    int num_vertices = 5;
+    topological_order(num_vertices, adjacency_matrix);
     return 0;
 }
